57f89ea438e9798bd27f3e714f085e32fc12c531
Yonatan Taragin <ytstuff@taragin.com>
2022-10-23 11:59:07 +0300

    Added Rest flow to step 6 and 7

--- solution/step6/protection/prot-svc.go
+++ solution/step6/protection/prot-svc.go
@@ -19,6 +19,7 @@ import (
 )
 
 var TUNNEL_URL string
+var TASKS_URL string
 var TASKS_GRPC_HOST string
 
 func getping(c *gin.Context) {
@@ -27,7 +28,7 @@ func getping(c *gin.Context) {
 	})
 }
 
-func UpdateTask(id string, status pb.TaskMessage_TaskStatus) error {
+func UpdateTaskGRPC(id string, status pb.TaskMessage_TaskStatus) error {
 	conn, err := grpc.Dial(TASKS_GRPC_HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
@@ -48,6 +49,32 @@ func UpdateTask(id string, status pb.TaskMessage_TaskStatus) error {
 	log.Printf("New Task Created %s with status %d", ret.GetTaskid(), ret.GetStatus())
 	return nil
 
+}
+
+func UpdateTaskRest(id string, status int) error {
+	log.Printf("Making REST call to update a task %s", id)
+
+	postBody := []byte(fmt.Sprintf(`{"status": %d}`, status))
+	reqBody := bytes.NewBuffer(postBody)
+
+	u := fmt.Sprintf("%s/task/%s", TASKS_URL, id)
+
+	req, err := http.NewRequest("PUT", u, reqBody)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.DefaultClient.Do(req)
+
+	//Handle Error
+	if err != nil {
+		log.Print("An Error Occured %v", err)
+		return err
+	}
+
+	log.Printf("Status Updated")
+
+	return nil
 }
 
 func callTunnelToCreateVPG(name string) (string, error) {
@@ -81,7 +108,7 @@ func callTunnelToCreateVPG(name string) (string, error) {
 
 }
 
-func createTask() (string, error) {
+func createTaskGRPC() (string, error) {
 	conn, err := grpc.Dial(TASKS_GRPC_HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
@@ -103,6 +130,35 @@ func createTask() (string, error) {
 	return ret.GetTaskid(), nil
 }
 
+func createTaskRest() (string, error) {
+
+	log.Printf("Making REST call to create a task")
+
+	resp, err := http.Post(TASKS_URL+"/task", "application/json", nil)
+	//Handle Error
+	if err != nil {
+		log.Print("An Error Occured %v", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+	//Read the response body\
+	decoder := json.NewDecoder(resp.Body)
+
+	type TaskResponse struct {
+		Taskid string `json:"taskid"`
+		Status int    `json:"status"`
+	}
+	var retobj TaskResponse
+	err = decoder.Decode(&retobj)
+	if err != nil {
+		log.Printf("Error decoding object %v", retobj)
+		return "", err
+	}
+
+	log.Printf("New Task Created %s with status %d", retobj.Taskid, retobj.Status)
+	return retobj.Taskid, nil
+}
+
 func createVPG(c *gin.Context) {
 	type VPGRequest struct {
 		VPGName string
@@ -117,7 +173,7 @@ func createVPG(c *gin.Context) {
 
 	log.Printf("Received request to create VPG %s", requestBody.VPGName)
 
-	taskid, err := createTask()
+	taskid, err := createTaskGRPC()
 	if err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"msg": "Unable to create task"})
 		return
@@ -130,7 +186,8 @@ func createVPG(c *gin.Context) {
 		return
 	}
 
-	err = UpdateTask(taskid, pb.TaskMessage_INPROGRESS)
+	// err = UpdateTaskGrpc(taskid, pb.TaskMessage_INPROGRESS)
+	err = UpdateTaskGRPC(taskid, 1)
 	if err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"msg": "Unable to create task"})
 		return
@@ -153,6 +210,7 @@ func main() {
 	log.Println("Protection Service starting up ")
 
 	TASKS_GRPC_HOST = getEnv("TASKSHOST", "tasks-grpc:9001")
+	TASKS_URL = getEnv("TASKSURL", "http://tasks-svc:8080")
 	TUNNEL_URL = getEnv("TUNNELURL", "http://tunnel-svc:8080")
 
 	r := gin.Default()
--- solution/step7/protection/prot-svc.go
+++ solution/step7/protection/prot-svc.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"time"
 
-	pb "protection/tasks" 
+	pb "protection/tasks"
 
 	"github.com/gin-gonic/gin"
 
@@ -20,6 +20,7 @@ import (
 )
 
 var TUNNEL_URL string
+var TASKS_URL string
 var TASKS_GRPC_HOST string
 
 func getping(c *gin.Context) {
@@ -71,7 +72,7 @@ func checkVPGAndUpdateTask(vpgid string, taskid string) bool {
 		return false
 	}
 
-	err = UpdateTask(taskid, pb.TaskMessage_COMPLETE)
+	err = UpdateTaskGRPC(taskid, pb.TaskMessage_COMPLETE)
 
 	return err == nil
 }
@@ -86,7 +87,7 @@ func pollAndCheckStatus(vpgid string, taskid string) {
 	log.Printf("VPG is complete %s", vpgid)
 }
 
-func UpdateTask(id string, status pb.TaskMessage_TaskStatus) error {
+func UpdateTaskGRPC(id string, status pb.TaskMessage_TaskStatus) error {
 	conn, err := grpc.Dial(TASKS_GRPC_HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
@@ -107,6 +108,32 @@ func UpdateTask(id string, status pb.TaskMessage_TaskStatus) error {
 	log.Printf("New Task Created %s with status %d", ret.GetTaskid(), ret.GetStatus())
 	return nil
 
+}
+
+func UpdateTaskRest(id string, status int) error {
+	log.Printf("Making REST call to update a task %s", id)
+
+	postBody := []byte(fmt.Sprintf(`{"status": %d}`, status))
+	reqBody := bytes.NewBuffer(postBody)
+
+	u := fmt.Sprintf("%s/task/%s", TASKS_URL, id)
+
+	req, err := http.NewRequest("PUT", u, reqBody)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.DefaultClient.Do(req)
+
+	//Handle Error
+	if err != nil {
+		log.Print("An Error Occured %v", err)
+		return err
+	}
+
+	log.Printf("Status Updated")
+
+	return nil
 }
 
 func callTunnelToCreateVPG(name string) (string, error) {
@@ -140,7 +167,7 @@ func callTunnelToCreateVPG(name string) (string, error) {
 
 }
 
-func createTask() (string, error) {
+func createTaskGRPC() (string, error) {
 	conn, err := grpc.Dial(TASKS_GRPC_HOST, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
@@ -162,6 +189,35 @@ func createTask() (string, error) {
 	return ret.GetTaskid(), nil
 }
 
+func createTaskRest() (string, error) {
+
+	log.Printf("Making REST call to create a task")
+
+	resp, err := http.Post(TASKS_URL+"/task", "application/json", nil)
+	//Handle Error
+	if err != nil {
+		log.Print("An Error Occured %v", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+	//Read the response body\
+	decoder := json.NewDecoder(resp.Body)
+
+	type TaskResponse struct {
+		Taskid string `json:"taskid"`
+		Status int    `json:"status"`
+	}
+	var retobj TaskResponse
+	err = decoder.Decode(&retobj)
+	if err != nil {
+		log.Printf("Error decoding object %v", retobj)
+		return "", err
+	}
+
+	log.Printf("New Task Created %s with status %d", retobj.Taskid, retobj.Status)
+	return retobj.Taskid, nil
+}
+
 func createVPG(c *gin.Context) {
 	type VPGRequest struct {
 		VPGName string
@@ -176,7 +232,7 @@ func createVPG(c *gin.Context) {
 
 	log.Printf("Received request to create VPG %s", requestBody.VPGName)
 
-	taskid, err := createTask()
+	taskid, err := createTaskGRPC()
 	if err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"msg": "Unable to create task"})
 		return
@@ -189,7 +245,8 @@ func createVPG(c *gin.Context) {
 		return
 	}
 
-	err = UpdateTask(taskid, pb.TaskMessage_INPROGRESS)
+	// err = UpdateTaskGrpc(taskid, pb.TaskMessage_INPROGRESS)
+	err = UpdateTaskGRPC(taskid, 1)
 	if err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"msg": "Unable to create task"})
 		return
@@ -214,6 +271,7 @@ func main() {
 	log.Println("Protection Service starting up ")
 
 	TASKS_GRPC_HOST = getEnv("TASKSHOST", "tasks-grpc:9001")
+	TASKS_URL = getEnv("TASKSURL", "http://tasks-svc:8080")
 	TUNNEL_URL = getEnv("TUNNELURL", "http://tunnel-svc:8080")
 
 	r := gin.Default()
